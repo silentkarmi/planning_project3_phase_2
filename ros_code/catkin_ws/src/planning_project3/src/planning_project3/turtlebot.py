f55f833d33c8351daeb32ebc30eaec3cdc92679e
Kartikeya Mishra <kmishra@umd.edu>
2022-04-28 10:59:28 -0400

    final commit to competition

--- ros_code/catkin_ws/src/planning_project3/src/planning_project3/turtlebot.py
+++ ros_code/catkin_ws/src/planning_project3/src/planning_project3/turtlebot.py
@@ -44,12 +44,12 @@ class TurtleBot:
          left_v, right_v = action            
          left_v, right_v = left_v / 100, right_v / 100
          
-         if TurtleBot.COUNTER > 88 and TurtleBot.COUNTER < 100:
-            if left_v == right_v:
-                left_v = right_v = 0
-                rospy.loginfo("Correcting for error")
+        #  if TurtleBot.COUNTER > 88 and TurtleBot.COUNTER < 100:
+        #     if left_v == right_v:
+        #         left_v = right_v = 0
+        #         rospy.loginfo("Correcting for error")
                 
-         if TurtleBot.COUNTER > 105 and TurtleBot.COUNTER < 135:
+         if TurtleBot.COUNTER > 105 and TurtleBot.COUNTER < 128:
             if left_v == right_v:
                 left_v = right_v = 0
                 rospy.loginfo("Correcting for error")
